04940516163343ce25fb46e8cb456e98313dc840
Elton Gao <gyfelton@gmail.com>
2012-11-12 10:37:58 -0500

    Initial commit

---
+++ UWAPIWrapper/UWAPIWrapper/FlipsideViewController.m
@@ -0,0 +1,36 @@
+//
+//  FlipsideViewController.m
+//  UWAPIWrapper
+//
+//  Created by Yuanfeng on 2012-11-12.
+//  Copyright (c) 2012 UW Mobile Club. All rights reserved.
+//
+
+#import "FlipsideViewController.h"
+
+@interface FlipsideViewController ()
+
+@end
+
+@implementation FlipsideViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Actions
+
+- (IBAction)done:(id)sender
+{
+    [self.delegate flipsideViewControllerDidFinish:self];
+}
+
+@end
---
+++ UWAPIWrapper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UWAPIWrapper
+//
+//  Created by Yuanfeng on 2012-11-12.
+//  Copyright (c) 2012 UW Mobile Club. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
